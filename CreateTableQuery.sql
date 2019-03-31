1d91c5e3cc089e52da9b923afb06c5bbf30f768e
Rohit Suryawanshi <rohit.suryawanshi@parambs.com>
2019-03-31 23:34:25 +0530

    Secound commit

---
+++ CreateTableQuery.sql
@@ -0,0 +1,7 @@
+CREATE TABLE pincodesofindia(
+   city     VARCHAR(50) NOT NULL
+  ,pincode        INTEGER  NOT NULL
+  ,taluk          VARCHAR(42) NOT NULL
+  ,district   VARCHAR(24) NOT NULL
+  ,state      VARCHAR(25) NOT NULL
+);
